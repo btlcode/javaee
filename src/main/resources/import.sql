0341e8545f49a5481452ed1ebbaf720b8ab72443
kara kartal <karakartal@mac>
2016-10-25 22:27:35 +0300

    .

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
 insert into parameter(id, name) values (1,'PARAM1')
 insert into parameter(id, name) values (2,'PARAM22')
 insert into parameter(id, name) values (3,'PARAM333')
+insert into parameter(id, name) values (4,'PARAM333')
